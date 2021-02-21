86d83b7060924f860e15d7c02f2ee04d8fd29a9a
sakurataiko1 <49112677+sakurataiko1@users.noreply.github.com>
2021-02-21 16:26:18 +0900

    Add files via upload

--- Qt_Cplus/test_Qcustomplot01/mainwindow.h
+++ Qt_Cplus/test_Qcustomplot01/mainwindow.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 
 #include <QTimer>
-#include "../../qcustomplot.h" // the header file of QCustomPlot. Don't forget to add it to your project, if you use an IDE, so it gets compiled.
+#include "qcustomplot.h" // the header file of QCustomPlot. Don't forget to add it to your project, if you use an IDE, so it gets compiled.
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
--- Qt_Cplus/test_Qcustomplot01/test_Qcustomplot01.pro
+++ Qt_Cplus/test_Qcustomplot01/test_Qcustomplot01.pro
@@ -12,13 +12,13 @@ TEMPLATE = app
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    qcustomplot.cpp \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    qcustomplot.cpp
 
 HEADERS += \
-    qcustomplot.h \
-    mainwindow.h
+    mainwindow.h \
+    qcustomplot.h
 
 FORMS += \
     mainwindow.ui
