e27e2a1d4e2ec0913efece8e73b6038b7c1af06a
sakurataiko1 <49112677+sakurataiko1@users.noreply.github.com>
2021-08-02 21:14:22 +0900

    Add files via upload

--- tmp/tmp_20210801/opengl_kuroda_rect02/main.cpp
+++ tmp/tmp_20210801/opengl_kuroda_rect02/main.cpp
@@ -1,11 +1,11 @@
-#include "window.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Window w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
